26a13169eed1fdbc9244b892388dd11343fb7409
muhamad.zolfaghari <you@example.com>
2023-10-19 20:18:38 +0330

    The nodemon and packages were added.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import * as express from 'express';
+import * as express from "express";
+
 const app = express();
 
-function main() {
+function defineConfig() {
 
+}
+
+function startServer() {
+  app.listen(process.env.PORT, () => {
+    console.log(`The server is started on port ${process.env.PORT}`);
+  });
+}
+
+function main() {
+  defineConfig();
+  startServer();
 }
 
 main();
